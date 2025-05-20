280c2c42309e409dc534b62856ea104039764d2d
KVNDW <kevin.wuersch@students.fhnw.ch>
2025-05-21 01:18:34 +0200

    aspect ratio iframe

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,5 +6,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const pdfFrame = document.getElementById("pdf-frame");
 
-    pdfFrame.src = `../resources/files/${pdfName}.pdf#view=FitH`;
+    pdfFrame.src = `../resources/files/${pdfName}.pdf#zoom=100`;
 });
